6dea03f81fa2d4b20b83a2cd292d8c71748f4f00
asaltcode <elangovanr558@gmail.com>
2024-07-05 06:11:38 -0700

    updatd

--- backend/src/config/fileUpload.js
+++ backend/src/config/fileUpload.js
@@ -2,6 +2,7 @@ import multer from 'multer';
 import path from 'path';
 import fs from 'fs';
 import { fileURLToPath } from 'url';
+import sharp from 'sharp';
 
 // Convert import.meta.url to a file path
 const __filename = fileURLToPath(import.meta.url);
@@ -37,3 +38,34 @@ export const upload = multer({
   limits: { fileSize: 1024 * 1024 * 5 }, // Limit file size to 5MB
   fileFilter: fileFilter
 });
+
+// Middleware to create a thumbnail
+export const createThumbnail = async (req, res, next) => {
+  if (!req.file) {
+    return next();
+  }
+
+  const thumbnailDir = path.join(__dirname, '../../images/thumbnails');
+
+  // Ensure the thumbnails directory exists
+  fs.mkdir(thumbnailDir, { recursive: true }, async (err) => {
+    if (err) {
+      return next(err);
+    }
+
+    const thumbnailPath = path.join(thumbnailDir, req.file.filename);
+
+    try {
+      await sharp(req.file.path)
+        .resize(35, 35) // Resize to 200x200 pixels
+        .toFile(thumbnailPath);
+
+      // Optionally, you can add the thumbnail path to the request object
+      req.file.thumbnailPath = thumbnailPath;
+
+      next();
+    } catch (err) {
+      next(err);
+    }
+  });
+};
--- backend/src/router/productRoutr.js
+++ backend/src/router/productRoutr.js
@@ -1,12 +1,13 @@
-import express from "express"
-import productController from "../controller/productController.js"
-import { upload } from "../config/fileUpload.js"
+import express from 'express';
+import productController from '../controller/productController.js';
+import { upload, createThumbnail } from '../config/fileUpload.js';
 
-const router = express.Router()
-router.post("/", productController.addProduct)
-router.get('/', productController.getProduct)
-router.get('/:id', productController.getProductById)
-router.put('/:id', upload.single('image'), productController.editProduct)
-router.delete('/:id', productController.deleteProduct)
+const router = express.Router();
 
-export default router
+router.post("/", productController.addProduct);
+router.get('/', productController.getProduct);
+router.get('/:id', productController.getProductById);
+router.put('/:id', upload.single('image'), createThumbnail, productController.editProduct);
+router.delete('/:id', productController.deleteProduct);
+
+export default router;
