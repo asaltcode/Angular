66c194bba2cdc3edfa73343add6b58a0fe2f7c33
asaltcode <elangovanr558@gmail.com>
2024-11-05 18:06:32 +0530

    updated

--- frontend/mynode.js
+++ frontend/mynode.js
@@ -2,7 +2,6 @@ const fs = require('fs');
 const path = require('path');
 const successColor = '\x1b[32m%s\x1b[0m';
 const checkSign = '\u{2705}';
-const dotenv = require('dotenv').config({path: 'src/.env'}); ;
 
 const envFile = `export const environment = {
     VARIABLE_NAME: '${process.env.VARIABLE_NAME}',
